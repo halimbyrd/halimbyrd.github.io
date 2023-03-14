b2506c4b6004bbfaebe6c4be80ef3f2e8a0894c0
Halim <abdulhalimibnbyrd@gmail.com>
2023-03-14 10:35:55 -0400

    finished NEW home section

--- HalimByrd/README.md
+++
@@ -1,1 +0,0 @@
-# HalimByrd
--- script.js
+++ script.js
@@ -1,46 +1,9 @@
+/*==================== toggle icon navbar ====================*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const hamburgerIcon = document.querySelector('#hamburger-icon');
-const xIcon = document.querySelector('#x-icon');
-
-const nav = document.querySelector('nav');
-
-
-
-
-
-
-hamburgerIcon.addEventListener('click', function() {
-
-   nav.style.display = 'block';
-   hamburgerIcon.style.display = 'none';
-   xIcon.style.display = 'block';
-
-
-})
-
-
-xIcon.addEventListener('click', function() {
-
-     nav.style.display = 'none';
-
-     hamburgerIcon.style.display = 'block';
-     xIcon.style.display = 'none';
-})
-
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
 

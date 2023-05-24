88e64f1df5c90b468113782108372edc8c846642
Halim <abdulhalimibnbyrd@gmail.com>
2023-05-24 01:14:30 -0400

    quick little update

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /*==================== typed js ====================*/
 const typed = new Typed('.multiple-text', {
-    strings: ['Software Engineer'],
+    strings: ['Web Developer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
